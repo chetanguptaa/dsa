18300aa70fbd64f890f3cccab2a18762dd3a064e
chetan gupta <chetangupta123raj@gmail.com>
2023-03-14 18:38:05 +0530

    updated cartesianProduct.js

--- cartesianProduct.js
+++ cartesianProduct.js
@@ -1,4 +1,4 @@
-function cartesianProduct(arr1, arr2) {
+const cartesianProduct = (arr1, arr2) => {
     const result = []
     for (let i = 0; i < arr1.length; i++) {
       for (let j = 0; j < arr2.length; j++) {
@@ -8,8 +8,8 @@ function cartesianProduct(arr1, arr2) {
     return result
 }
   
-var arr1 = [1, 2]
-var arr2 = [3, 4, 5]
+let arr1 = [1, 2]
+let arr2 = [3, 4, 5]
 console.log(cartesianProduct(arr1, arr2)) // [[1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5]]
   
     
